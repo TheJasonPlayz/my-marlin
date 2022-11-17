73cbbec8b87a698fcb970d7997eb59dd21b7b7e5
TheJasonPlayz <thejasonplayz@gmail.com>
2022-11-17 12:48:33 -0700

    Changed values
    Changed preheat values

--- Marlin-1.1.9.1 (Fresh)/Marlin/Configuration.h
+++ Marlin-1.1.9.1 (Fresh)/Marlin/Configuration.h
@@ -1260,9 +1260,9 @@
 // @section temperature
 
 // Preheat Constants
-#define PREHEAT_1_TEMP_HOTEND 190
+#define PREHEAT_1_TEMP_HOTEND 210
 #define PREHEAT_1_TEMP_BED     60
-#define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
+#define PREHEAT_1_FAN_SPEED     255 // Value from 0 to 255
 
 #define PREHEAT_2_TEMP_HOTEND 240
 #define PREHEAT_2_TEMP_BED     90
