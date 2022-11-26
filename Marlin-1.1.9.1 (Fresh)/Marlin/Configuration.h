0e8199a6b78d43c4f64385491010f23c24fd2dce
TheJasonPlayz <thejasonplayz@gmail.com>
2022-11-26 13:52:08 -0700

    Update Configuration.h
    Updated probe offsets from extruder
    Updated bed positions, will commit with more updates because of new offsets.

--- Marlin-1.1.9.1 (Fresh)/Marlin/Configuration.h
+++ Marlin-1.1.9.1 (Fresh)/Marlin/Configuration.h
@@ -780,9 +780,9 @@
  *      O-- FRONT --+
  *    (0,0)
  */
-#define X_PROBE_OFFSET_FROM_EXTRUDER 3   // X offset: -left  +right  [of the nozzle]
-#define Y_PROBE_OFFSET_FROM_EXTRUDER 6   // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -2    // Z offset: -below +above  [the nozzle]
+#define X_PROBE_OFFSET_FROM_EXTRUDER 30   // X offset: -left  +right  [of the nozzle]
+#define Y_PROBE_OFFSET_FROM_EXTRUDER 55  // Y offset: -front +behind [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -2     // Z offset: -below +above  [the nozzle] // + means nozzle prints higher, - means nozzle prints lower
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE -10
@@ -1028,8 +1028,8 @@
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Set the boundaries for probing (where the probe can reach).
-  #define LEFT_PROBE_BED_POSITION MIN_PROBE_EDGE
-  #define RIGHT_PROBE_BED_POSITION (X_BED_SIZE - 19.5)
+  #define LEFT_PROBE_BED_POSITION -MIN_PROBE_EDGE
+  #define RIGHT_PROBE_BED_POSITION (X_BED_SIZE - 5.5)
   //#define FRONT_PROBE_BED_POSITION MIN_PROBE_EDGE
   #define BACK_PROBE_BED_POSITION (Y_BED_SIZE - 55)
 
